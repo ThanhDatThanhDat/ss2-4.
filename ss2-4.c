5d4d8a036d38bdc5da669fda0ea0f35d38d21cdc
ThanhDatThanhDat <dt2229992@gmail.com>
2024-11-19 22:37:33 +0700

    ss2-4

---
+++ ss2-4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	// khai bao bien
+	int edge = 2;
+	
+	// Tinh chu vi va in ra
+	int perimeter = 4 * edge;
+	printf("Ket qua perimiter la %d \n", perimeter);
+	
+	
+	// Tinh dien tich va in ra
+	int acreage = edge * edge;
+	printf("Ket qua acreage la %d\n", acreage);
+	
+	return 0; 
+}
